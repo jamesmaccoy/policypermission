f126395ee879357a88187fc0682dde893dae33a6
jamesmaccoy <thankyou.digital@gmail.com>
2025-01-17 12:27:28 +0200

    ii

---
+++ src/collections/Policys.ts
@@ -0,0 +1,27 @@
+import { CollectionConfig } from "payload/types";
+import { isAdmin } from "../access/isAdmin";
+import { isAdminOrHasPolicyAccess } from "../access/isAdminOrHasPolicyAccess";
+
+export const Policys: CollectionConfig = {
+  slug: 'policys',
+  admin: {
+    useAsTitle: 'title',
+  },
+  access: {
+    // Only admins can create
+    create: isAdmin,
+    // Only admins or editors with policy access can read
+    read: isAdminOrHasPolicyAccess('id'),
+    // Only admins can update
+    update: isAdmin,
+    // Only admins can delete
+    delete: isAdmin,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    }
+  ]
+}
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -11,6 +11,8 @@ import { Media } from './collections/Media'
 import { Pages } from './collections/Pages'
 import { Posts } from './collections/Posts'
 import { Users } from './collections/Users'
+import { Policys } from './collections/Policys'
+import { Pockets } from './collections/Pockets'
 import { Footer } from './Footer/config'
 import { Header } from './Header/config'
 import { plugins } from './plugins'
@@ -62,7 +64,7 @@ export default buildConfig({
   db: mongooseAdapter({
     url: process.env.DATABASE_URI || '',
   }),
-  collections: [Pages, Posts, Media, Categories, Users],
+  collections: [Policys, Pockets, Pages, Posts, Media, Categories, Users],
   cors: [getServerSideURL()].filter(Boolean),
   globals: [Header, Footer],
   plugins: [
